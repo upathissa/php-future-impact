548198f32bea15d4821949ca88ed78a8705b8814
upathissa <amilaupathissa@gmail.com>
2018-07-04 21:35:30 +0530

    basic layout

---
+++ inc/sidebar.php
@@ -0,0 +1,18 @@
+<section id="sidebar">
+
+    <!-- Intro -->
+        <?php require_once 'template-parts/intro.php'; ?>
+
+    <!-- Mini Posts -->
+        <?php require_once 'template-parts/main_posts.php'; ?>
+        
+    <!-- Posts List -->
+        <?php require_once 'template-parts/post_lists.php'; ?>
+        
+    <!-- About -->
+        <?php require_once 'template-parts/about.php'; ?>
+
+    <!-- Footer -->
+        <?php require_once 'template-parts/page_footer.php'; ?>
+
+</section>
